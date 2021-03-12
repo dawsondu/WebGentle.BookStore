3392e28eb5e42b007bd016b942282cf9832f471a
dawsondu <453963739@qq.com>
2021-03-12 12:08:41 +0800

    add jquery and bootsrapusestaticfiles(), self-defined files path.

--- WebGentle.BookStore/Controllers/HomeController.cs
+++ WebGentle.BookStore/Controllers/HomeController.cs
@@ -8,9 +8,18 @@ namespace WebGentle.BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
     }
 }
