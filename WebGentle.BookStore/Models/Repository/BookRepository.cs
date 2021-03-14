3dbc8165706ad35d87e02d72cb440ca6132c4658
Dawson <dawson.dyx@gmail.com>
2021-03-14 21:58:14 +0800

    Add _ViewStartadd book details and similar books

--- WebGentle.BookStore/Models/Repository/BookRepository.cs
+++ WebGentle.BookStore/Models/Repository/BookRepository.cs
@@ -25,11 +25,12 @@ namespace WebGentle.BookStore.Models.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){ Id=1, Title="MVC",Author="Nitish"},
-                new BookModel(){ Id=2, Title="Dot Net Core",Author="Nitish"},
-                new BookModel(){ Id=3, Title="C#",Author="Monika"},
-                new BookModel(){ Id=4, Title="Java",Author="WebGentle"},
-                new BookModel(){ Id=5, Title="Php",Author="WebGentle"},
+                new BookModel(){ Id=1, Title="MVC",Author="Nitish", Description="This is a book about MVC",Category="Action",Language="English", TotalPages=1245},
+                new BookModel(){ Id=2, Title="Dot Net Core",Author="Nitish", Description="This is a book about dot net Core", Category="Drama",Language="Chinese", TotalPages=1003},
+                new BookModel(){ Id=3, Title="C#",Author="Monika",  Description="This is a book about C Sharp",Category="Tech",Language="Irish",TotalPages = 562},
+                new BookModel(){ Id=4, Title="Java",Author="WebGentle",Description="This is a book about Java",Category="Tech",Language="India",TotalPages = 145},
+                new BookModel(){ Id=5, Title="Php",Author="WebGentle",Description="This is a book about PHP" ,Category="Action",Language="English",TotalPages = 568},
+                new BookModel(){ Id=6, Title="JSP",Author="WebGentle",Description="This is a book about JSP",Category="Tech",Language="Janpan",TotalPages = 689},
             };
         }
     }
