c9fb359975cbb4fde4a0f9186fe5964a979ddd2f
dawsondu <453963739@qq.com>
2021-03-18 15:24:32 +0800

    Validation Summary

--- WebGentle.BookStore/Models/BookModel.cs
+++ WebGentle.BookStore/Models/BookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,15 @@ namespace WebGentle.BookStore.Models
     public class BookModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter the title of your book.")]
         public string Title { get; set; }
+        [Required(ErrorMessage ="Please enter your name.")]
         public string Author { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string Language { get; set; }
+        [Required]
+        [Display(Name ="Total pages of book")]
         public int TotalPages { get; set; }
     }
 }
